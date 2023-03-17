2ae3a816ec8ce04b8c77eed8fd185d6478952a8a
ANNEQWACUKA <annewacuka0@gmail.com>
2023-03-17 03:42:47 -0700

    4 c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 \**
 *main-this is main function
 *
 *Return:always 0
 *\
-		init main (void)
+                int main (void)
 {
         puts("\"Programming is like building a multilingual puzzle);
-	return(0);
+        return(0);
 }
