f0b351e8268aa1d169aa0c8d6a591d936acc5033
ANNEQWACUKA <annewacuka0@gmail.com>
2023-03-27 05:12:36 -0700

    1 c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function interchanges integers
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
