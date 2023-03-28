ead9324e35c98962ff66fb9e7f07fd01755f9772
ANNEQWACUKA <annewacuka0@gmail.com>
2023-03-28 03:57:16 -0700

    pointers 5th c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,17 @@
 #include "main.h"
 /**
  * rev_string - reverses a string
+ * @s: parameter function
  *
- *
- *Return: should be 0
+ * Return: should be 0
  */
 void rev_string(char *s)
 {
+	int i;
+	int count = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = count; i >= 0; i--)
+		_putchar(s[i]);
 }
